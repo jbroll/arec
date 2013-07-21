b43ab70f708b831ef417cf5c6c11568a6733f9df
John Roll <john@rkroll.com>
2013-07-21 14:38:27 -0400

    arec-extend

--- arec-extend.tcl
+++ arec-extend.tcl
@@ -1,12 +1,19 @@
 
-# ARec extention signature:
-#
-#    method(Tcl_Interp* ip, ARecType type, void *recs, int nrec, objv, objc)
-
-#critcl::api ARecRegisterMethod(type, name, function);
-
 namespace eval arec {}
 
+int ARecRegisterMethod(char *type, char *name, int flags)
+{
+    if ( !strcmp(type, "arec") ) {
+	ARecAddMethod(ARecMethods, name, flags);
+    } else {
+	if ( (type = ARecLookupType(type)) == NULL ) {
+	    return TCL_ERROR;
+	}
+
+	ARecAddMethod(type->methods, name, flags);
+    }
+    return TCL_OK;
+}
 
 # type	  - Type of ARec to add method to.  If type is "arec" add method as general method
 #           available to all types.
@@ -20,16 +27,35 @@ namespace eval arec {}
 # body	  - C code.
 #
 proc arec::cmethod { type name args rettype body } {
+    variable UnPack
 
-    set code {}
-    critcl::cproc arec::$name $args $rettype
+    lassign [critcl::name2c $name] ns cns name cname
+
+    critcl::cproc $name $args $rettype "$UnPack\n$body" -pass-cdata 1 -register no
+    critcl::cinit "ARecRegisterMethod(\"$type\", tcl_$cns$cname, 0);" {}
 }
+
+    critcl::ccode {
+	typedef struct _ARecCD {
+	    void *inst;
+	    void *recs;
+	    int   nrec;
+	} ARecCD;
+    }
+
+    set arec::UnPack {
+	/* unpack the ARec ClientData */
+	ARecCD *__arec_cd = (ARecCD*) clientdata;
+
+	void *inst = __arec_cd->type;
+	void *recs = __arec_cd->recs;
+	int  *nrec = __arec_cd->nrec;
+    }
+
 
 arec::cmethod arec dodo { int x int y } int {
     return x*y;
 }
 
 package provide arec-extend 1.0
-
-
 
